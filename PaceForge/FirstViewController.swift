ccd13f34033b4b852f2b9f683ed375c3dc85e202
brianoleary <brian.oleary15@gmail.com>
2018-04-11 11:12:33 -0400

    Initial Commit

---
+++ PaceForge/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  PaceForge
+//
+//  Created by Brian O'Leary on 4/11/18.
+//  Copyright © 2018 Brian Thomas O'Leary. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ PaceForge/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  PaceForge
+//
+//  Created by Brian O'Leary on 4/11/18.
+//  Copyright © 2018 Brian Thomas O'Leary. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
